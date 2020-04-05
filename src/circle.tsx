10047cb385fa8bee47afa1433c2053229517deb0
Michal Ciesliczka <mciesliczka@utilitywarehouse.co.uk>
2020-04-05 14:53:03 +0200

    breaking change: add customization for arcs

--- src/circle.tsx
+++ src/circle.tsx
@@ -6,15 +6,21 @@ const CONVERT_COEFFICIENT = 2 * PI / 360;
 
 interface Props {
   radius: number,
-  intervals: Array<AnglesDescriptor>,
+  intervals: Array<Intervals>,
   width: number,
-  color: string,
+  defaultColor: string,
   backgroundColor: string,
 }
 
 interface Point {
   x: number,
   y: number,
+}
+
+interface Intervals {
+  angles: AnglesDescriptor,
+  onPress?: () => void,
+  color?: string,
 }
 
 interface AnglesDescriptor {
@@ -55,7 +61,7 @@ const getPointsByDescriptor = (angles: AnglesDescriptor, center: Point, radius: 
 const MultiArcCircle = ({
   radius,
   intervals,
-  color,
+  defaultColor,
   backgroundColor,
   width
 }: Props) => {
@@ -63,23 +69,24 @@ const MultiArcCircle = ({
   const internalRadius = radius - width;
   const center = { x: radius, y: radius };
 
-  const paths = intervals.map(angles => {
+  const paths = intervals.map(({ angles, onPress, color = defaultColor }) => {
     const { start: startPoint, end: endPoint, middle: middlePoint }
       = getPointsByDescriptor(angles, center, radius);
     const { start: startPointInternal, end: endPointInternal, middle: middlePointInternal }
       = getPointsByDescriptor(angles, center, internalRadius);
 
     return angles.end - angles.start <= 180 ?
-      <Path key={`${angles.start}.${angles.end}`} d={`M ${startPoint.x} ${startPoint.y} 
+      <Path key={`${angles.start}.${angles.end}`} d={`M ${startPoint.x} ${startPoint.y}
         A ${radius} ${radius} , 0, 0, 1, ${endPoint.x} ${endPoint.y}
         A ${EDGE_RADIUS} ${EDGE_RADIUS} , 0, 0, 1, ${endPointInternal.x} ${endPointInternal.y}
         A ${internalRadius} ${internalRadius} , 0, 0, 0, ${startPointInternal.x} ${startPointInternal.y}
         A ${EDGE_RADIUS} ${EDGE_RADIUS} , 0, 0, 1, ${startPoint.x} ${startPoint.y}
       `}
         fill={color}
+        onPress={onPress}
       />
       :
-      <Path key={`${angles.start}.${angles.end}`} d={`M ${startPoint.x} ${startPoint.y} 
+      <Path key={`${angles.start}.${angles.end}`} d={`M ${startPoint.x} ${startPoint.y}
         A ${radius} ${radius} , 0, 0, 1, ${middlePoint && middlePoint.x} ${middlePoint && middlePoint.y}
         A ${radius} ${radius} , 0, 0, 1, ${endPoint.x} ${endPoint.y}
         A ${EDGE_RADIUS} ${EDGE_RADIUS} , 0, 0, 1, ${endPointInternal.x} ${endPointInternal.y}
@@ -88,6 +95,7 @@ const MultiArcCircle = ({
         A ${EDGE_RADIUS} ${EDGE_RADIUS} , 0, 0, 1, ${startPoint.x} ${startPoint.y}
       `}
         fill={color}
+        onPress={onPress}
       />
   })
 
@@ -101,16 +109,16 @@ const MultiArcCircle = ({
       position: 'absolute',
       borderRadius: radius,
     }} width={radius * 2} height={radius * 2}>
-      <Path d={`M ${startPoint.x} ${startPoint.y} 
+      <Path d={`M ${startPoint.x} ${startPoint.y}
     A ${radius} ${radius} , 0, 0, 1, ${endPoint.x} ${endPoint.y}
     L ${endPointInternal.x} ${endPointInternal.y}
     A ${internalRadius} ${internalRadius} , 0, 0, 0, ${startPointInternal.x} ${startPointInternal.y}
-    L ${startPoint.x} ${startPoint.y} 
+    L ${startPoint.x} ${startPoint.y}
 
     A ${radius} ${radius} , 0, 0, 0, ${endPoint.x} ${endPoint.y}
     L ${endPointInternal.x} ${endPointInternal.y}
     A ${internalRadius} ${internalRadius} , 0, 0, 1, ${startPointInternal.x} ${startPointInternal.y}
-    L ${startPoint.x} ${startPoint.y} 
+    L ${startPoint.x} ${startPoint.y}
     `}
         fill={backgroundColor}
       />
